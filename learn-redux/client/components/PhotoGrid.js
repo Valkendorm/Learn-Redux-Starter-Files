37943c0fe144f7575242b33cac51f504c76dbab1
Martin Asnong <martin.asnong@ubisoft.com>
2018-06-07 08:09:32 -0400

    Completed course

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router";
+
+import Photo from "./Photo";
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, index) => (
+          <Photo key={index} index={index} post={post} {...this.props} />
+        ))}
+      </div>
+    );
+  }
+});
+
+export default PhotoGrid;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,39 @@
-// let's go!
+import React from "react";
+import { render } from "react-dom";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { Provider } from "react-redux";
+import Raven from "raven-js";
+//import { sentry_url, logException } from "./data/config";
+
+import store, { history } from "./store";
+import App from "./components/App";
+import PhotoGrid from "./components/PhotoGrid";
+import Single from "./components/Single";
+
+import css from "./styles/style.styl";
+
+/*
+Raven.config(sentry_url, {
+  tags: {
+    git_commit: "assadas9564",
+    userLevel: "peon"
+  }
+}).install();
+
+logException(new Error("download failed"), {
+  email: "bob@bob.com"
+});
+*/
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid} />
+        <Route path="/view/:postId" component={Single} />
+      </Route>
+    </Router>
+  </Provider>
+);
+
+render(router, document.getElementById("root"));
